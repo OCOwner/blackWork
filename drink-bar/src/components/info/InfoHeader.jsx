a7b9066d7f817409f9267d809e8551ec89ebdbcf
김효환 <gyghks1017@gmail.com>
2024-06-26 16:37:37 +0900

    [Feat] : 인포헤더 백그라운드이미지 변경

--- drink-bar/src/components/info/InfoHeader.jsx
+++ drink-bar/src/components/info/InfoHeader.jsx
@@ -6,6 +6,7 @@ import backgroundImage from '../../assets/image/background-image.png'; // 배경
 const InfoHeader = () => {
   return (
     <Container>
+      <Overlay />
       <Title>강남 착석바</Title>
       <Subtitle>BAR11 CLUB</Subtitle>
       <ImageContainer>
@@ -21,6 +22,7 @@ const InfoHeader = () => {
 export default InfoHeader;
 
 const Container = styled.div`
+  position: relative; /* 부모 요소 기준으로 자식 요소 위치 지정 */
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -34,42 +36,57 @@ const Container = styled.div`
   background-position: center; // 배경 이미지 가운데 정렬
 `;
 
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* 어두운 배경 색상 설정 */
+  filter: blur(8px); /* 배경 이미지 흐리게 처리 */
+`;
+
 const Title = styled.h1`
   font-weight: 900;
   font-size: 65px;
   color: white; // 배경 이미지와 대비되는 색상 설정
+  z-index: 1; /* 텍스트가 오버레이 위에 나타나도록 설정 */
 `;
 
 const Subtitle = styled.h2`
   font-weight: 600;
   font-size: 40px;
   color: white; // 배경 이미지와 대비되는 색상 설정
+  z-index: 1; /* 텍스트가 오버레이 위에 나타나도록 설정 */
 `;
 
 const ImageContainer = styled.div`
+  position: relative; /* 자식 요소의 위치 설정을 위해 부모 요소도 relative로 설정 */
   display: flex;
   align-items: center;
   margin-top: 20px;
   width: 100%;
   justify-content: center;
   gap: 20px;
+  z-index: 1; /* 이미지가 오버레이 위에 나타나도록 설정 */
 `;
 
 const StyledImage = styled.img`
-  width: 50px;
+  width: 80px;
   height: auto;
   border-radius: 10px;
 `;
 
 const Line = styled.div`
   height: 2px;
-  background-color: black;
+  background-color: red;
   flex-grow: 1;
-  max-width: 300px;
+  max-width: 500px;
 `;
 
 const QA = styled.p`
   font-weight: 900;
   font-size: 65px;
   color: white; // 배경 이미지와 대비되는 색상 설정
+  z-index: 1; /* 텍스트가 오버레이 위에 나타나도록 설정 */
 `;
