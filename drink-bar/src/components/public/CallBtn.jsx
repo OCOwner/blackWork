81108ea35be7e343d506b6a584e6352c651c8a19
김효환 <gyghks1017@gmail.com>
2024-06-26 15:29:24 +0900

    [Feat] : 전화문의 버튼 생성

---
+++ drink-bar/src/components/public/CallBtn.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CallBtn = () => {
+  const phoneNumber = '010-5691-6578'; // 전화번호를 여기에 설정
+
+  return (
+    <ButtonWrapper>
+      <StyledButton href={`tel:${phoneNumber}`}>전화 문의</StyledButton>
+    </ButtonWrapper>
+  );
+};
+
+export default CallBtn;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+`;
+
+const StyledButton = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
+  background-color: red;
+  border: none;
+  border-radius: 30px;
+  text-decoration: none;
+  transition: background-color 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+
+  &:active {
+    background-color: #004085;
+  }
+
+  & > svg {
+    margin-right: 10px;
+  }
+`;
--- drink-bar/src/pages/Info.jsx
+++ drink-bar/src/pages/Info.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import InfoHeader from '../components/info/InfoHeader';
 import Review from '../components/info/Review';
+import CallBtn from '../components/public/CallBtn';
 
 const Info = () => {
   return (
     <div>
       <InfoHeader />
       <Review />
+      <CallBtn />
     </div>
   );
 };
